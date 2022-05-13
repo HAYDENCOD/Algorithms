fbe32cc38051c08e46999d49d072cbf133876fbe
Paul Webo <paulwebo38@gmail.com>
2022-05-13 15:27:42 +0300

    iterative fibonacci sequence

---
+++ src/fibonacci.java
@@ -0,0 +1,20 @@
+public class fibonacci {
+    public static void fib(int n){
+        int b = 0;
+        int a = 1;
+        int c;
+        System.out.print(a + " ");
+        System.out.print(b + " ");
+        for (int i = 0; i < n; i++) {
+            c = a + b;
+            System.out.print(c +" ");
+            b = a;
+            a =c;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        fib(10);
+    }
+}
