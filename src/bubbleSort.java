92c366df7c7cefa93d801fb8535bb4a295ddaf68
Paul Webo <paulwebo38@gmail.com>
2022-05-13 10:57:12 +0300

    bubble sort

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -1,8 +1,8 @@
 public class bubbleSort {
     public static void bubbleSort(int[] arr){
         int max = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length -1 - i; j++) {
                 int temp;
                 if (arr[j] > arr[j +1]){
                     temp = arr[j];
