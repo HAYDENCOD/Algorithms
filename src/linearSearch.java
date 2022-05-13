8d0e7df77c265414e6be4bacbf3c98894929e972
Paul Webo <paulwebo38@gmail.com>
2022-05-13 10:51:29 +0300

    Linear search

--- src/linearSearch.java
+++ src/linearSearch.java
@@ -2,7 +2,7 @@ public class linearSearch {
     public static void linearSearch(int[] arr, int key){
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == key){
-                System.out.println("found");
+                System.out.println("Found");
             }
         }
     }
